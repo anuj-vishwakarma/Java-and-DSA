928eab407028b605cb97203aa05a2b4bd617ffa2
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2024-02-28 17:40:14 +0530

    Add files via upload

---
+++ DSA/Day3/FindingTargetInInfiniteArrayAmazon.java
@@ -0,0 +1,42 @@
+
+//           https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
+// approach : we've given an infinite array so we won't be using array.length
+            // 1. make a box of start = 0 and end = 1;
+            // 2. Keep doubling the size of box until target > arr[end] because if target > arr[end]
+            // then it will  not be lesser than arr[start] or lie int the range start and end
+            //
+
+public class FindingTargetInInfiniteArrayAmazon {
+    public static void main(String[] args) {
+        int [] arr= {2, 3,5, 6, 7, 12,64};
+        int target = 7;
+        System.out.println(ans(arr, target));
+    }
+    static int ans (int []arr, int target){
+        int start = 0;
+        int end = 1;
+        while(target>arr[end]){
+            int temp = end  + 1;// this is my new start
+//          end = previous end + sizeOfBox * 2;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start,end);
+    }
+    static int binarySearch (int [] arr, int target, int start, int end) {
+
+
+        while(start <= end ) {
+            int mid = start + (end - start)  / 2;
+            if(target < arr[mid]) {
+                end = mid - 1;
+            }else if (target > arr[mid]) {
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
