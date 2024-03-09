7d52f54036f54fb772a1df58a92c93d699c44c46
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2024-03-09 23:54:02 +0530

    Update Patter13Striver.java

--- DSA/Day 11/Patter13Striver.java
+++ DSA/Day 11/Patter13Striver.java
@@ -3,10 +3,21 @@ public class Patter13Striver {
         pattern(5);
     }
     static void pattern (int n ){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
             }
+            System.out.println();
+
         }
     }
 }
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
