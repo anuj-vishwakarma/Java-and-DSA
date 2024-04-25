b9895a71cbcb7a6be35773107802641d4e600ea4
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2024-04-25 08:38:18 +0530

    Deleted OOPS practice because was not in directory

--- Java/Object-Oriented-Programming/Regex.java
+++
@@ -1,6 +0,0 @@
-public class Regex {
-    public static void main(String[] args) {
-        String str = "32:";
-        System.out.println(str.matches("\\d*:"));
-    }
-}
--- Java/Object-Oriented-Programming/Substring.java
+++
@@ -1,9 +0,0 @@
-public class Substring {
-    public static void main(String[] args) {
-        String str = "24-10-2003";
-        System.out.println(str.substring(0,2));
-        System.out.println(str.substring(str.indexOf('-')+1,str.lastIndexOf('-')));
-        System.out.println(str.substring(str.lastIndexOf('-')+1));
-
-    }
-}
